d8ac4ce3f8163ede5840472d8c52ea451b69b147
Bùi Thanh Ngân <44390877+sadboizaintcry@users.noreply.github.com>
2025-06-25 13:50:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def handleRegister(session):
         #if registrationResponse["message"] == "An email has been sent with verification link, please check your email inbox to verify your account.":
             #logging.info(f"{registrationResponse['message']} -> {USER_DATA['email']}")
         registrationResponse = true
-        if(registrationResponse)
+        if registrationResponse:
             authToken = None
             
             for attempt in range(1, 4):
